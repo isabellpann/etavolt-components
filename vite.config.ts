4c27e862e935e09834af218ea112a94c05a77419
isabellpann <131356207+herenotforlongbutforclasspart@users.noreply.github.com>
2024-08-29 16:59:47 +0800

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: '@etavolt-etatune/button',
+        replacement: path.resolve(__dirname, 'node_modules/@etavolt-etatune/button/src'),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
   },
-})
+});
