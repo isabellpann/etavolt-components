0690e1cd829fc8cae15c12e8c131fe3e028f41b4
isabellpann <131356207+herenotforlongbutforclasspart@users.noreply.github.com>
2024-08-29 16:31:48 +0800

    fixed directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 import Power from "./components/ui/icons/power"
 import SolarType from "./components/ui/icons/solar_type"
 import Angle from "./components/ui/icons/angle"
@@ -6,18 +6,22 @@ import Date from "./components/ui/icons/date"
 import RoofArea from "./components/ui/icons/roof_area"
 import Hashtag from "./components/ui/icons/hashtag"
 
+import {Button} from "@etavolt-etatune/button";
+
+
 function App() {
 
   return (
     <div>
-    <Button variant={"secondary"} className="ml-40 mr-40 mt-40">Secondary</Button>
-    <Button variant={"default"} className="mt-40">Default</Button>
+    {/* <Button variant={"secondary"} className="ml-40 mr-40 mt-40">Secondary</Button>
+    <Button variant={"default"} className="mt-40">Default</Button> */}
     <Power/>
     <SolarType/>
     <Angle/>
     <Date/>
     <RoofArea/>
     <Hashtag/>
+    <Button variant={"secondary"}/>
   </div>
   )
 }
