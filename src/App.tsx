f020385f7f9e6496e66f97a3eab4a0eabf41ab06
isabellpann <131356207+herenotforlongbutforclasspart@users.noreply.github.com>
2024-08-29 14:03:34 +0800

    created default buttons

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+    <Button variant={"secondary"} className="ml-40 mr-40 mt-40">Secondary</Button>
+    <Button variant={"default"} className="mt-40">Default</Button>
+  </div>
+  )
+}
+
+export default App
