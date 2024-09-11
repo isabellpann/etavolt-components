029743c49b32afb54f521f2df77ae69699b2aaa3
isabellpann <131356207+herenotforlongbutforclasspart@users.noreply.github.com>
2024-09-11 14:15:55 +0800

    drop-down menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,68 @@
-import Power from "./components/ui/icons/power"
-import SolarType from "./components/ui/icons/solar_type"
-import Angle from "./components/ui/icons/angle"
-import Date from "./components/ui/icons/date"
-import RoofArea from "./components/ui/icons/roof_area"
-import Hashtag from "./components/ui/icons/hashtag"
-import InfoIconButton from "./components/ui/icons/info"
-import { Button } from "./components/ui/button"
-import {Checkbox} from "./components/ui/checkbox"
+import Power from "./components/ui/icons/power";
+import SolarType from "./components/ui/icons/solar_type";
+import Angle from "./components/ui/icons/angle";
+import Date from "./components/ui/icons/date";
+import RoofArea from "./components/ui/icons/roof_area";
+import Hashtag from "./components/ui/icons/hashtag";
+import InfoIconButton from "./components/ui/icons/info";
+import { EtavoltButton } from "./components/ui/button";
+import { Checkbox } from "./components/ui/checkbox";
+import {
+  EtavoltDropdownMenu,
+  EtavoltDropdownMenuContent,
+  EtavoltDropdownMenuItem,
+  EtavoltDropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState<string>("Type of PV Panels Installed");
+
+  const handleSelect = (item: string) => {
+    setSelectedItem(item);
+  };
+
+  useEffect(() => {
+    console.log(selectedItem);
+  }, [selectedItem]);
 
   return (
     <div>
-    <Button variant={"secondary"} className="ml-40 mr-40 mt-40">Secondary</Button>
-    <Button variant={"default"} className="mt-40">Default</Button>
-    <Power/>
-    <SolarType/>
-    <Angle/>
-    <InfoIconButton/>
-    <Date />
+      <EtavoltButton variant={"secondary"} className="ml-40 mr-40 mt-40">
+        Secondary
+      </EtavoltButton>
+      <EtavoltButton variant={"default"} className="mt-40">
+        Default
+      </EtavoltButton>
+      <Power />
+      <SolarType />
+      <Angle />
+      <InfoIconButton />
+      <Date />
 
-    <Checkbox className="ml-40"/>
-    <RoofArea/>
-    <Hashtag/>
-    <Button variant={"secondary"} disabled>Secondary disabled</Button>
-    <Button variant={"default"} className="ml-40" disabled>Default disabled</Button>
+      <Checkbox className="ml-40" />
+      <RoofArea />
+      <Hashtag />
+      <EtavoltButton variant={"secondary"} disabled>
+        Secondary disabled
+      </EtavoltButton>
+      <EtavoltButton variant={"default"} className="ml-40" disabled>
+        Default disabled
+      </EtavoltButton>
 
-  </div>
-  )
+      <EtavoltDropdownMenu>
+        <EtavoltDropdownMenuTrigger className="ml-40 mt-40 mb-40"  isDefault={selectedItem === "Type of PV Panels Installed"}>{selectedItem}</EtavoltDropdownMenuTrigger>
+        <EtavoltDropdownMenuContent>
+          <EtavoltDropdownMenuItem onSelect={() => {handleSelect('Modern Standard')}}
+            checked={selectedItem === 'Modern Standard'} >Modern Standard</EtavoltDropdownMenuItem>
+          <EtavoltDropdownMenuItem onSelect={() => {handleSelect('Older Non-anti-reflective Coated')}}
+            checked={selectedItem === 'Older Non-anti-reflective Coated'} >Older Non-anti-reflective Coated</EtavoltDropdownMenuItem>
+          <EtavoltDropdownMenuItem onSelect={() => {handleSelect('Thin Film')}}
+            checked={selectedItem === 'Thin Film'} >Thin Film</EtavoltDropdownMenuItem>
+        </EtavoltDropdownMenuContent>
+      </EtavoltDropdownMenu>
+    </div>
+  );
 }
 
-export default App
+export default App;
